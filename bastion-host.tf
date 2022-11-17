78bae008014f90e9fda84640bfc4b7228c4864f0
Nitesh Bhardwaj <nitesh1408922outlook.com>
2022-11-18 01:42:43 +0530

    some fixes

--- bastion-host.tf
+++ bastion-host.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "nitesh-bastion-server" {
-  ami                    = lookup(var.AMIS, var.region )
+  ami                    = lookup(var.AMIS, var.region)
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.nitesh-key.key_name
   subnet_id              = module.vpc.public_subnets[0]
   count                  = var.instance_count
   vpc_security_group_ids = [aws_security_group.nitesh-bastion-sg.id]
-  tags                   = {
+  tags = {
     Name    = "nitesh-bastion-server"
     Project = var.projectName
   }
@@ -13,7 +13,7 @@ resource "aws_instance" "nitesh-bastion-server" {
   provisioner "file" {
     content = templatefile("templates/db-deploy.tmpl", {
       rds-endpoint = aws_db_instance.nitesh-rds.address, dbuser = var.dbuser, dbpass = var.dbpass
-    } )
+    })
     destination = "/tmp/nitesh-dbdeploy.sh"
   }
 
